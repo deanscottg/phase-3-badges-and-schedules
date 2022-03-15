22062daf86c9afd92f8ce6c9a5dddf5768618a44
Dean <deanscottg@gmail.com>
2022-03-15 11:17:00 -0400

    commit tree 01e8c71512ceaa67bb9171771a7a58ceafd72d8c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(attendee)
+      "Hello, my name is #{attendee}."
+end
+
+def batch_badge_creator(attendees_array)
+    attendees_array.map do |attendee|
+     badge_maker(attendee)
+    end
+
+end
+
+def assign_rooms(speakers)
+    rooms_aray = []
+    speakers.each_with_index do |speaker, index|
+     rooms_aray.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!") 
+    
+    end 
+    rooms_aray
+end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each do |badge|
+    puts badge
+    end
+    assign_rooms(attendees).each do |room|
+        puts room
+end
+end
